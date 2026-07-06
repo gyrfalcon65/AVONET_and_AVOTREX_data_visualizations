b882022273d7275d984191b08194199ad93df40b
albatross101 <Andrew.tao.sunshine.111@gmail.com>
2026-07-05 22:57:46 -0400

    Made a bar graph for flight ability

--- AVOTREX_visualization.py
+++ AVOTREX_visualization.py
@@ -7,10 +7,21 @@ df = pd.read_csv("data/AVOTREX.csv")
 order_counts = df["order"].value_counts()
 order_proportions_AVOTREX = df["order"].value_counts(normalize=True)
 
+#Flight ability counts
+flight_ability_counts = df["flight_ability"].value_counts()
+print(flight_ability_counts)
+
 plt.bar(order_proportions_AVOTREX.index, order_proportions_AVOTREX.values)
 plt.xlabel("Order")
 plt.ylabel("Proportion")
 plt.title("Order Proportions of Extinct Birds")
 plt.xticks(rotation=90)
+plt.tight_layout()
+plt.show()
+
+plt.bar(flight_ability_counts.index.astype(str), flight_ability_counts.values, width=0.4)
+plt.xlabel("Flight Ability")
+plt.ylabel("Number of Species")
+plt.title("Flight Ability of Extinct Birds")
 plt.tight_layout()
 plt.show()
